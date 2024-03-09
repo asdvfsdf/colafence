964c1df501c8bbef0496b0d6a9b9d13675d57a6e
cola <1952088712@qq.com>
2024-03-09 15:53:32 +0800

    make output file ok

--- sharedlib/colafence.c
+++ sharedlib/colafence.c
@@ -11,86 +11,86 @@ static void before(void) __attribute__((constructor));
 static void before()
 {
     printf("before main\n");
-    // analysis_dir = getenv("ANALYSIS");
-    // // init_timer();
-    // init_fence_output();
+    analysis_dir = getenv("ANALYSIS");
+    // init_timer();
+    init_fence_output();
 }
 
 int malloc_hook_active = 1;
 
-// struct sigaction sa;
-// struct itimerval timer;
+struct sigaction sa;
+struct itimerval timer;
 
-// unsigned long *pool;
+unsigned long *pool;
 
-// void hook_enable(){
-//     malloc_hook_active = 1;
+void hook_enable(){
+    malloc_hook_active = 1;
 
-//     timer.it_value.tv_sec = 0;    // 0秒后触发
-//     timer.it_value.tv_usec = INTERVAL; // 500毫秒
-//     timer.it_interval.tv_sec = 0;  // 间隔时间为0，表示不重复定时
-//     timer.it_interval.tv_usec = 0;
-//     setitimer(ITIMER_REAL, &timer, NULL);
-// }
+    timer.it_value.tv_sec = 0;    // 0秒后触发
+    timer.it_value.tv_usec = INTERVAL; // 500毫秒
+    timer.it_interval.tv_sec = 0;  // 间隔时间为0，表示不重复定时
+    timer.it_interval.tv_usec = 0;
+    setitimer(ITIMER_REAL, &timer, NULL);
+}
 
-// void init_timer(){
-//     // 注册定时器信号处理函数
-//     sa.sa_handler = hook_enable;
-//     sigemptyset(&sa.sa_mask);
-//     sa.sa_flags = 0;
-//     sigaction(SIGALRM, &sa, NULL);
+void init_timer(){
+    // 注册定时器信号处理函数
+    sa.sa_handler = hook_enable;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGALRM, &sa, NULL);
 
-//     // 设置定时器
-//     timer.it_value.tv_sec = 0; // 5秒后触发
-//     timer.it_value.tv_usec = INTERVAL;
-//     timer.it_interval.tv_sec = 0;
-//     timer.it_interval.tv_usec = 0;
+    // 设置定时器
+    timer.it_value.tv_sec = 0; // 5秒后触发
+    timer.it_value.tv_usec = INTERVAL;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
 
-//     // 激活定时器
-//     setitimer(ITIMER_REAL, &timer, NULL);
+    // 激活定时器
+    setitimer(ITIMER_REAL, &timer, NULL);
 
-// }
+}
 
-// int init_fence_output(){  
+int init_fence_output(){  
 
-//     if(access(analysis_dir, F_OK) == 0){
+    if(access(analysis_dir, F_OK) == 0){
 
-//     }else{
+    }else{
 
-//         int result = mkdir(analysis_dir, 0777);
+        int result = mkdir(analysis_dir, 0777);
 
-//         if (result == 0) {
-//             printf("dir init success\n");
-//         } else {
-//             printf("dir init failed\n");
-//             perror("Error ");
-//             exit(EXIT_FAILURE);
-//         }
+        if (result == 0) {
+            printf("dir init success\n");
+        } else {
+            printf("dir init failed\n");
+            perror("Error ");
+            exit(EXIT_FAILURE);
+        }
 
-//     }
+    }
 
-//     char pid[10];
-//     sprintf(pid, "%d", (int)getpid());
+    char pid[10];
+    sprintf(pid, "%d", (int)getpid());
 
-//     output_file = fopen(strcat(strcat(analysis_dir, "/"), pid), "w+");
+    output_file = fopen(strcat(strcat(analysis_dir, "/"), pid), "w+");
 
-//     if (output_file != NULL) {
-//         fprintf(output_file, "colafence output\n");
-//     } else {
-//         exit(EXIT_FAILURE);
-//     }
+    if (output_file != NULL) {
+        fprintf(output_file, "colafence output\n");
+    } else {
+        exit(EXIT_FAILURE);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
-// int init_pool(){
+int init_pool(){
 
-//     pool = (unsigned long *)malloc(PAGE_SIZE * DEFENCE);
+    pool = (unsigned long *)malloc(PAGE_SIZE * DEFENCE);
     
-//     if(!pool){
-//       exit(EXIT_FAILURE);
-//     }
-// }
+    if(!pool){
+      exit(EXIT_FAILURE);
+    }
+}
 
 
 
